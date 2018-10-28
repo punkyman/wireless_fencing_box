0df5ae958f4dab55e214f5531f70799a18c06dce
fabrizio.larato@gmail.com <fabrizio.larato@gmail.com>
2018-10-28 17:36:48 +0100

    Fixed wrong pipe id

--- wireless_fencing_box/code/emetteur/src/main.cpp
+++ wireless_fencing_box/code/emetteur/src/main.cpp
@@ -32,8 +32,8 @@
 #define ID_PIN 8
 
 // NOTE: the "LL" at the end of the constant is "LongLong" type
-const uint64_t pipe_left = 0xDEADBEEF; // Define the transmit pipe
-const uint64_t pipe_right = 0xBEEFDEAD; // Define the transmit pipe
+const uint64_t pipe_left = 0xBEEFDEAD01; // Define the transmit pipe
+const uint64_t pipe_right = 0xBEEFDEAD02; // Define the transmit pipe
 
 int sender_id;
 
